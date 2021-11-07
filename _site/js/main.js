703f5cb418cbea51cb0c5eb332f2dca789ccf227
Jonathan Dobres <jdobres@Jonathans-MacBook-Pro.local>
2021-11-07 13:34:09 -0500

    keyboard navigation for image galleries

--- _site/js/main.js
+++ _site/js/main.js
@@ -78,4 +78,33 @@ $(document).ready(function() {
 		$('#lightbox').toggleClass('active')
 		$('#lightbox .content').css('background', 'none')
 	})
+	
+	// keyboard controls
+	window.addEventListener("keydown", function (event) {
+		
+		if (!$('#lightbox').hasClass('active')) {
+			return
+		}
+		  
+		  if (event.key == 'Esc' || event.key == 'Escape') {
+			  event.preventDefault()
+			  $('#lightbox').toggleClass('active')
+			  $('#lightbox .content').css('background', 'none')
+		  }
+		  
+		  if (event.key == 'ArrowRight' || event.key == 'Right') {
+			  event.preventDefault()
+			  current_index = (current_index + 1) % gallery_images.length
+			  let this_image = gallery_images[current_index]
+			  set_lightbox(this_image.width, this_image.height, this_image.current_source)
+		  }
+		  
+		  if (event.key == 'ArrowLeft' || event.key == 'Left') {
+			  event.preventDefault()
+			  current_index = (current_index - 1) % gallery_images.length
+			  current_index = current_index == -1 ? gallery_images.length - 1 : current_index
+			  let this_image = gallery_images[current_index]
+			  set_lightbox(this_image.width, this_image.height, this_image.current_source)
+		  } 
+	})
 })
